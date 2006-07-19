391d432c42b75e3ac2194fe147a2171d9f1f5857
Ryan Heaton <ryan@webcohesion.com>
2006-07-19 22:06:31 +0000

    beginning integration of xfire.
    git-svn-id: https://svn.codehaus.org/enunciate/trunk/enunciate@41 c2b6a248-b21b-0410-9d80-82131e002806

---
+++ src/samples/basic/net/sf/enunciate/samples/petclinic/services/impl/PetServiceImpl.java
@@ -0,0 +1,44 @@
+package net.sf.enunciate.samples.petclinic.services.impl;
+
+import net.sf.enunciate.samples.petclinic.Pet;
+import net.sf.enunciate.samples.petclinic.PetType;
+import net.sf.enunciate.samples.petclinic.services.PetService;
+import net.sf.enunciate.samples.petclinic.services.ServiceException;
+
+import javax.jws.WebService;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ryan Heaton
+ */
+@WebService (
+  endpointInterface = "net.sf.enunciate.samples.petclinic.services.PetService"
+)
+public class PetServiceImpl implements PetService {
+
+  private static Map<Integer, Pet> PETS = Collections.synchronizedMap(new HashMap<Integer, Pet>());
+
+  static {
+    for (int i = 1; i <= 9; i++) {
+      Pet pet = new Pet();
+      pet.setId(i);
+      GregorianCalendar calendar = new GregorianCalendar(2000, 1, i);
+      pet.setName(String.format("%tA", calendar));
+      PetType[] petTypes = PetType.values();
+      pet.setType(petTypes[i % petTypes.length]);
+      pet.setBirthDate(calendar.getTime());
+      PETS.put(i, pet);
+    }
+  }
+
+  public Pet readPet(int id) throws ServiceException {
+    return PETS.get(id);
+  }
+
+  public void storePet(Pet pet) throws ServiceException {
+    PETS.put(pet.getId(), pet);
+  }
+}
