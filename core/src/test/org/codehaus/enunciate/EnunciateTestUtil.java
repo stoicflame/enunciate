a2520bd7609d848b146c404d118eed771b507d0e
Ryan Heaton <ryan@webcohesion.com>
2007-03-15 17:35:23 +0000

    adding the copyright notice to all the files.
    git-svn-id: https://svn.codehaus.org/enunciate/trunk/enunciate@172 c2b6a248-b21b-0410-9d80-82131e002806

--- core/src/test/org/codehaus/enunciate/EnunciateTestUtil.java
+++ core/src/test/org/codehaus/enunciate/EnunciateTestUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Ryan Heaton
+ * Copyright 2006 Web Cohesion
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/samples/wannabecool/step4/com/ifyouwannabecool/api/package-info.java
+++ src/samples/wannabecool/step4/com/ifyouwannabecool/api/package-info.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2006 Web Cohesion
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * <p>Welcome to the API for our incredibly innovative social networking site,
  * <u>IfYouWannaBeCool.com</u>!  We've got a full SOAP API, and even a few
