75e46f449766e8c9a808ea3783531526c558e973
Richard Easterling <reaster3@gmail.com>
2008-03-06 02:04:41 +0000

    added json and rest tests
    git-svn-id: https://svn.codehaus.org/enunciate/trunk/enunciate@353 c2b6a248-b21b-0410-9d80-82131e002806

--- src/samples/petclinic/src/test/java/org/codehaus/enunciate/samples/petclinic/services/PetclinicIntegrationTest.java
+++ src/samples/petclinic/src/test/java/org/codehaus/enunciate/samples/petclinic/services/PetclinicIntegrationTest.java
@@ -1,149 +1,104 @@
 package org.codehaus.enunciate.samples.petclinic.services;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.util.StringTokenizer;
-
-import javax.xml.XMLConstants;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.JXPathIntrospector;
 import org.apache.log4j.Logger;
-import org.codehaus.enunciate.samples.petclinic.services.Clinic;
 import org.codehaus.enunciate.test.integration.GenericEnunciateTestCase;
-import org.json.JSONArray;
+import org.jdom.Document;
 import org.json.JSONObject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-//import org.w3c.dom.Document;
-//import org.w3c.dom.NamedNodeMap;
-//import org.w3c.dom.Node;
 
+/**
+ * Sample integration tests using Jetty as a embedded test container.
+ *
+ * @author reaster
+ */
 public class PetclinicIntegrationTest extends GenericEnunciateTestCase {
 
-	private static Logger logger = Logger.getLogger(PetclinicIntegrationTest.class);
+       private static Logger logger = Logger.getLogger(PetclinicIntegrationTest.class);
 
-	public static final String GET_OWNER_NOUN = "owner";
+       public static final String GET_OWNER_NOUN = "owner";
 
-	static protected String WS_NAME = "clinicService";
-
-	static Clinic clinic = null;
-
-//	static Clinic getClinic() {
-//		if (clinic == null) {
-//			clinic = new ClinicImpl(getSoapEndpoint(WS_NAME));
-//		}
-//		return clinic;
-//	}
+//      static protected String WS_NAME = "clinicService";
+//      static Clinic clinic = null;
 //
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		GenericEnunciateTestCase.setUpBeforeClass();
-		//Object obj = GenericEnunciateTestCase.getWebContextBean("clinic");
-		//ClinicImpl serviceImpl = (ClinicImpl)obj;
-		//logger.info("found ClinicDefault in WebContext: " + obj);
-		// serviceImpl.setExampleManager( new ExampleManagerMock());
-	}
+//      static Clinic getClinic() {
+//              if (clinic == null) {
+//                      clinic = new ClinicImpl(getSoapEndpoint(WS_NAME));
+//              }
+//              return clinic;
+//      }
 
-	@AfterClass
-	public static void tearDownAfterClass() {
-		GenericEnunciateTestCase.tearDownAfterClass();
-	}
+       @BeforeClass
+       public static void setUpBeforeClass() throws Exception {
+               //GenericEnunciateTestCase.setRelativeContext("/petclinic-1.0-SNAPSHOT");
+               //GenericEnunciateTestCase.setWebappHome(new java.io.File("target/petclinic-1.0-SNAPSHOT"));
+               GenericEnunciateTestCase.setUpBeforeClass();
+               addDefaultNamespaces("ns0", "http://enunciate.codehaus.org/petclinic");
 
-	@Test
-	public void testServiceIsRunning() throws Exception {
-		GetMethod method = new GetMethod(getBaseContextAsString());
-		try {
-			int statusCode = httpClient.executeMethod(method);
-			assertEquals("HttpStatus.SC_OK", HttpStatus.SC_OK, statusCode);
-			byte[] responseBody = method.getResponseBody();
-			assertNotNull("body read succesfully", responseBody);
-		} finally {
-			method.releaseConnection();
-		}
-	}
+               //Object obj = GenericEnunciateTestCase.getWebContextBean("clinic");
+               //ClinicImpl serviceImpl = (ClinicImpl)obj; get ClassCastException cus in different classloaders
+               //logger.info("found ClinicDefault in WebContext: " + obj);
+               // serviceImpl.setExampleManager( new ExampleManagerMock());
+       }
 
-//	@Test
-//	public void testRestReadExampleType() throws Exception {
-//		addDefaultNamespaces("xsi",
-//				XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI, "xs",
-//				XMLConstants.W3C_XML_SCHEMA_NS_URI, "ns1",
-//				"http://connectapi.garmin.com/1.0_SNAPSHOT/example-service-data");
-//		String rest = getRestEndpoint(EXAMPLE_TYPE_NOUN) + "/99";
-//		GetMethod method = new GetMethod(rest);
-//		try {
-//			int statusCode = httpClient.executeMethod(method);
-//			assertEquals("HttpStatus.SC_OK", HttpStatus.SC_OK, statusCode);
-//			Document xml = parseXml(method.getResponseBodyAsStream());
-//			String idValue = xPathValue(xml, "//id");
-//			assertEquals("contains id", "99", idValue);
-//			String valueValue = xPathValue(xml, "//value");
-//			assertEquals("contains id", "true", valueValue);
-//			// TODO get XPath to work using namespaces
-//			// String xsiTypeValue = xPathValue(xml, "//value/@xsi:type");
-//			// assertEquals("contains value type", "xs:boolean", xsiTypeValue);
-//			Node xsiTypeNode = xPathNode(xml, "//value");
-//			NamedNodeMap attrs = xsiTypeNode.getAttributes();
-//			Node xsiAttrValue = attrs.getNamedItem("xsi:type");
-//			String xsiTypeValue2 = xsiAttrValue.getNodeValue();
-//			assertEquals("contains value type", "xs:boolean", xsiTypeValue2);
-//		} finally {
-//			method.releaseConnection();
-//		}
-//	}
-//
-//	@Test
-//	public void testJsonReadExampleType() throws Exception {
-//		String rest = getJsonEndpoint(EXAMPLE_TYPE_NOUN) + "/99";
-//		// result:
-//		// {"ns1.exampleType":{"id":"99","value":{"@xsi.type":"xs:boolean"}}}
-//		GetMethod method = new GetMethod(rest);
-//		try {
-//			int statusCode = httpClient.executeMethod(method);
-//			assertEquals("HttpStatus.SC_OK", HttpStatus.SC_OK, statusCode);
-//			// testing using strings
-//			String body = method.getResponseBodyAsString();
-//			assertTrue("contains id", body.indexOf("id\":\"99") > 1);
-//			assertTrue("contains value", body
-//					.indexOf("\"@xsi.type\":\"xs:boolean\"") > 1);
-//			// testing using JSONObject
-//			JSONObject jsonObj = new JSONObject(body);
-//			Object result = poorMansJXPath(jsonObj,
-//					"ns1.exampleType/value/@xsi.type");
-//			assertEquals("@xsi.type == xs:boolean", "xs:boolean", result);
-//			// testing using jXPathValue helper method
-//			Object xsiTypeValue = jXPathValue(jsonObj,
-//					"./ns1.exampleType/value[@name='@xsi.type']");
-//			assertEquals("@xsi.type == xs:boolean", "xs:boolean", xsiTypeValue);
-//			// testing using JXPathContext
-//			JXPathContext context = JXPathContext.newContext(jsonObj);
-//			Object xsiTypeValue2 = context
-//					.getValue("./ns1.exampleType/value[@name='@xsi.type']");
-//			assertEquals("@xsi.type == xs:boolean", "xs:boolean", xsiTypeValue2);
-//		} finally {
-//			method.releaseConnection();
-//		}
-//	}
-//
-//	@Test
-//	public void testReadExampleTypeSoapClient() {
-//		// we are testing this instance: ExampleType("99", Boolean.TRUE);
-//		ExampleType exampleType = getClinic().readExampleType("99");
-//		assertNotNull("exampleType not null", exampleType);
-//		assertEquals("value == TRUE", Boolean.TRUE, exampleType.getValue());
-//	}
-//
-//	@Test
-//	public void testReadExampleTypesSoapClient() {
-//		ExampleTypes exampleTypes = getClinic().readExampleTypes();
-//		assertNotNull(exampleTypes);
-//		assertFalse(exampleTypes.getList().isEmpty());
-//	}
+       @AfterClass
+       public static void tearDownAfterClass() {
+               GenericEnunciateTestCase.tearDownAfterClass();
+       }
+
+       @Test
+       public void testServiceIsRunning() throws Exception {
+               GetMethod method = new GetMethod(getBaseContextAsString());
+               try {
+                       int statusCode = httpClient.executeMethod(method);
+                       assertEquals("HttpStatus.SC_OK", HttpStatus.SC_OK, statusCode);
+                       byte[] responseBody = method.getResponseBody();
+                       assertNotNull("body read succesfully", responseBody);
+               } finally {
+                       method.releaseConnection();
+               }
+       }
+
+       @Test
+       public void testRestGetOwnerById() throws Exception {
+               //<ns0:owner xmlns:ns0="http://enunciate.codehaus.org/petclinic" id="1"><address>3 Third Street, Suite 0</address><city>Orlando, FL</city><firstName>Sally</firstName><lastName>Gates</lastName><telephone>111-1111, x0</telephone><petIds><petId>16</petId></petIds></ns0:owner>
+               GetMethod method = new GetMethod(getRestEndpoint(GET_OWNER_NOUN)+"/1");
+               try {
+                       int statusCode = httpClient.executeMethod(method);
+                       assertEquals("HttpStatus.SC_OK", HttpStatus.SC_OK, statusCode);
+                       Document xml = parseXml(method.getResponseBodyAsStream());
+                       //new XMLOutputter().output(xml, System.out);
+                       assertNotNull("body read succesfully", xml);
+                       assertEquals("id==1", "1", xPathValue(xml, "/ns0:owner/@id"));
+                       assertEquals("firstName==Sally", "Sally", xPathValue(xml, "/ns0:owner/firstName"));
+               } finally {
+                       method.releaseConnection();
+               }
+       }
+
+       @Test
+       public void testJsonGetOwnerById() throws Exception {
+               //{"ns0.owner":{"@id":"1","address":"3 Third Street, Suite 0","city":"Orlando, FL","firstName":"Sally","lastName":"Gates","telephone":"111-1111, x0","petIds":{"petId":16}}}
+               GetMethod method = new GetMethod(getJsonEndpoint(GET_OWNER_NOUN)+"/1");
+               try {
+                       int statusCode = httpClient.executeMethod(method);
+                       assertEquals("HttpStatus.SC_OK", HttpStatus.SC_OK, statusCode);
+                       String json = method.getResponseBodyAsString();
+                       logger.info(json);
+                       JSONObject jsonObj = new JSONObject(json);
+                       JXPathContext context = JXPathContext.newContext(jsonObj);
+                       assertEquals("id==1", "1", context.getValue("./ns0.owner[@name='@id']"));
+                       assertEquals("firstName==Sally", "Sally", context.getValue("./ns0.owner/firstName"));
+               } finally {
+                       method.releaseConnection();
+               }
+       }
 
 }
