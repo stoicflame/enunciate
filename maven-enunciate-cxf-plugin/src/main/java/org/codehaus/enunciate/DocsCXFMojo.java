689e783ea56c1a8a44f17d1add65852d899fa88b
Ryan Heaton <ryan@webcohesion.com>
2011-12-13 12:37:51 -0700

    ENUNCIATE-604: docs mojos for cxf, jboss maven plugins

---
+++ maven-enunciate-cxf-plugin/src/main/java/org/codehaus/enunciate/DocsCXFMojo.java
@@ -0,0 +1,11 @@
+package org.codehaus.enunciate;
+
+/**
+ * @goal docs
+ * @phase process-sources
+ * @extendsPlugin enunciate
+ *
+ * @author Ryan Heaton
+ */
+public class DocsCXFMojo extends DocsMojo {
+}
