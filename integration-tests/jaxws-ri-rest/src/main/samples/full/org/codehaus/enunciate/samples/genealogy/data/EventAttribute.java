d39731f581b464e9096f7e169f9b93b70bff2e1b
Ryan Heaton <ryan@webcohesion.com>
2012-08-03 11:09:30 -0600

    @XmlSeeAlso the impl

--- integration-tests/jaxws-ri-rest/src/main/samples/full/org/codehaus/enunciate/samples/genealogy/data/EventAttribute.java
+++ integration-tests/jaxws-ri-rest/src/main/samples/full/org/codehaus/enunciate/samples/genealogy/data/EventAttribute.java
@@ -1,8 +1,11 @@
 package org.codehaus.enunciate.samples.genealogy.data;
 
+import javax.xml.bind.annotation.XmlSeeAlso;
+
 /**
  * @author Ryan Heaton
  */
+@XmlSeeAlso(EventAttributeImpl.class)
 public interface EventAttribute {
 
   String getName();
