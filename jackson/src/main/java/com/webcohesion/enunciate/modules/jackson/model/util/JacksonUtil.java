132b5d69670dbeea1271564c7c7c2b23780c685f
Ryan Heaton <ryan@webcohesion.com>
2015-05-29 10:47:14 -0600

    import optimization

--- jackson/src/main/java/com/webcohesion/enunciate/modules/jackson/model/util/JacksonUtil.java
+++ jackson/src/main/java/com/webcohesion/enunciate/modules/jackson/model/util/JacksonUtil.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.databind.util.Converter;
 import com.webcohesion.enunciate.EnunciateException;
 import com.webcohesion.enunciate.javac.decorations.Annotations;
 import com.webcohesion.enunciate.javac.decorations.DecoratedProcessingEnvironment;
-import com.webcohesion.enunciate.javac.decorations.TypeMirrorDecorator;
 import com.webcohesion.enunciate.javac.decorations.type.DecoratedDeclaredType;
 import com.webcohesion.enunciate.javac.decorations.type.DecoratedTypeMirror;
 import com.webcohesion.enunciate.javac.decorations.type.TypeMirrorUtils;
@@ -34,10 +33,9 @@ import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.ArrayType;
 import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.MirroredTypeException;
 import javax.lang.model.type.TypeMirror;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import java.util.*;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 /**
