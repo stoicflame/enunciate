709b0cf67a92ac2f1801d82c750c0fdce451a0da
Ryan Heaton <ryan@webcohesion.com>
2015-08-21 14:59:57 -0600

    updating php-xml tests

---
+++ php-xml-client/src/test/samples/com/webcohesion/enunciate/examples/php_xml_client/schema/vehicles/BusType.java
@@ -0,0 +1,14 @@
+package com.webcohesion.enunciate.examples.php_xml_client.schema.vehicles;
+
+import com.webcohesion.enunciate.metadata.qname.XmlQNameEnum;
+
+/**
+ * @author Ryan Heaton
+ */
+@XmlQNameEnum (namespace = "")
+public enum BusType {
+
+  charter,
+
+  school
+}
