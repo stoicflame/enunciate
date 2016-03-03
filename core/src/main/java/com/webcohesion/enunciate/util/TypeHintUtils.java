44948804b8de596704c537f92cfd21c20d71f241
Ryan Heaton <ryan@webcohesion.com>
2016-03-03 14:54:40 -0700

    TypeHint.NO_CONTENT getting rendered as a data type per #261

--- core/src/main/java/com/webcohesion/enunciate/util/TypeHintUtils.java
+++ core/src/main/java/com/webcohesion/enunciate/util/TypeHintUtils.java
@@ -1,10 +1,12 @@
 package com.webcohesion.enunciate.util;
 
 import com.webcohesion.enunciate.javac.decorations.TypeMirrorDecorator;
+import com.webcohesion.enunciate.javac.decorations.type.DecoratedTypeMirror;
 import com.webcohesion.enunciate.metadata.rs.TypeHint;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.MirroredTypeException;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
@@ -41,7 +43,28 @@ public class TypeHintUtils {
       }
     }
     catch (MirroredTypeException e) {
-      typeMirror = TypeMirrorDecorator.decorate(e.getTypeMirror(), env);
+      DecoratedTypeMirror decorated = (DecoratedTypeMirror) TypeMirrorDecorator.decorate(e.getTypeMirror(), env);
+      if (decorated.isInstanceOf(TypeHint.NO_CONTENT.class)) {
+        typeMirror = env.getTypeUtils().getNoType(TypeKind.VOID);
+      }
+      else if (decorated instanceof DeclaredType){
+        String hintName = ((TypeElement)((DeclaredType)decorated).asElement()).getQualifiedName().toString();
+
+        if (decorated.isInstanceOf(TypeHint.NONE.class)) {
+          hintName = hintInfo.qualifiedName();
+        }
+
+        if (!"##NONE".equals(hintName)) {
+          TypeElement type = env.getElementUtils().getTypeElement(hintName);
+          typeMirror = TypeMirrorDecorator.decorate(env.getTypeUtils().getDeclaredType(type), env);
+        }
+        else {
+          typeMirror = defaultValue;
+        }
+      }
+      else {
+        typeMirror = decorated;
+      }
     }
     return typeMirror;
   }
