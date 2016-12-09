351f4caaaefc72e67cbc16f09509e4d79c04985d
Ryan Heaton <ryan@webcohesion.com>
2016-12-09 15:31:06 -0700

    Strip quotes in path segments when using spring shortcut annotations. Fixes #533.

--- spring-web/src/main/java/com/webcohesion/enunciate/modules/spring_web/model/SpringController.java
+++ spring-web/src/main/java/com/webcohesion/enunciate/modules/spring_web/model/SpringController.java
@@ -284,8 +284,8 @@ public class SpringController extends DecoratedTypeElement implements HasFacets 
                     Object value = entry.getValue().getValue();
                     if (value instanceof List) {
                       for (int i = 0; i < ((List) value).size(); i++) {
-                        Object valueItem = ((List) value).get(i);
-                        subpaths.add(valueItem.toString());
+                        AnnotationValue valueItem = (AnnotationValue) ((List) value).get(i);
+                        subpaths.add(String.valueOf(valueItem.getValue()));
                       }
                     }
                   }
