7d566f210e88d4f22060086f26440e913d3d3fa5
Loïc B <lbndev@yahoo.fr>
2016-12-09 17:40:13 +0100

    Types detection : auto-add child types of returned types. Fixes #558.
    This will only happen if no @SeeAlso or @JsonSubTypes annotation is present on the parent type.
    Interfaces are not handled, only base classes.

--- jackson1/src/main/java/com/webcohesion/enunciate/modules/jackson1/EnunciateJackson1Context.java
+++ jackson1/src/main/java/com/webcohesion/enunciate/modules/jackson1/EnunciateJackson1Context.java
@@ -543,6 +543,37 @@ public class EnunciateJackson1Context extends EnunciateModuleContext implements 
         stack.pop();
       }
     }
+
+    if (subTypes == null && seeAlso == null && declaration instanceof TypeElement) {
+      // No annotation tells us what to do, so we'll look up subtypes and add them
+      Types typeUtils = getContext().getProcessingEnvironment().getTypeUtils();
+      String declarationTypeName = ((TypeElement) declaration).getQualifiedName().toString();
+      try {
+        for (Element el : getContext().getApiElements()) {
+          if (el instanceof TypeElement) {
+            TypeElement te = (TypeElement) el;
+            TypeElement superEl = (TypeElement) typeUtils.asElement(te.getSuperclass());
+            if (superEl != null && declarationTypeName.equals(superEl.getQualifiedName().toString())) {
+              add(createTypeDefinition(te), stack);
+            }
+          }
+        }
+      } catch (MirroredTypeException e) {
+        TypeMirror mirror = e.getTypeMirror();
+        Element element = typeUtils.asElement(mirror);
+        if (element instanceof TypeElement) {
+          add(createTypeDefinition((TypeElement) element), stack);
+        }
+      } catch (MirroredTypesException e) {
+        List<? extends TypeMirror> mirrors = e.getTypeMirrors();
+        for (TypeMirror mirror : mirrors) {
+          Element element = typeUtils.asElement(mirror);
+          if (element instanceof TypeElement) {
+            add(createTypeDefinition((TypeElement) element), stack);
+          }
+        }
+      }
+    }
   }
 
   /**
