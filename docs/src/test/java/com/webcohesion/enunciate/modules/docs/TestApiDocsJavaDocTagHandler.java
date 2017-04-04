97e02c8d6ebe28e8cebcaf5b024af1d0dc1a918a
Loïc B <lbndev@yahoo.fr>
2017-04-04 19:22:57 +0200

    Debug & improvements on raw link detection

--- docs/src/test/java/com/webcohesion/enunciate/modules/docs/TestApiDocsJavaDocTagHandler.java
+++ docs/src/test/java/com/webcohesion/enunciate/modules/docs/TestApiDocsJavaDocTagHandler.java
@@ -13,10 +13,17 @@ public class TestApiDocsJavaDocTagHandler extends TestCase {
     assertFalse(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("<a href=http://blah.com>blah</a>").find());
     assertFalse(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("<a href=\"http://blah.com\">http://blah.com</a>").find());
     assertFalse(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("<a href=\"http://blah.com\">the thing for http://blah.com</a>").find());
+    assertFalse(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("<img src=\"http://blah.com/img.png\"/>").find());
+    assertFalse(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("Some inline image <img src=\"http://blah.com/img.png\"/> in the middle of text").find());
+    assertTrue(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("http://blah.com").find());
+    assertTrue(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("Something to see on http://blah.com").find());
+    assertTrue(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("http://blah.com has more info").find());
+    assertTrue(ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("Just in case, http://blah.com has more info").find());
     Matcher rawLink = ApiDocsJavaDocTagHandler.RAW_LINK_PATTERN.matcher("You can see http://blah.com for more info");
     assertTrue(rawLink.find());
     assertEquals("http://blah.com", rawLink.group(1));
-    assertEquals("You can see <a href=\"http://blah.com\">http://blah.com</a> for more info", rawLink.replaceAll(" <a href=\"$1\">$1</a> "));
+    assertEquals("You can see <a href=\"http://blah.com\">http://blah.com</a> for more info", rawLink.replaceAll(" <a href=\"$1\">$1</a>"));
+    assertEquals("You can see <a target=\"_blank\" href=\"http://blah.com\">http://blah.com</a> for more info", rawLink.replaceAll(" <a target=\"_blank\" href=\"$1\">$1</a>"));
   }
 
 }
