a15f09c347d2c54ec64a7d0f518499e48c96402b
ncoe <nathan.coe@sirsidynix.com>
2017-09-20 21:16:57 -0600

    Removed description from file downloads that are not for artifacts that have been attached.

---
+++ docs/src/test/samples/com/webcohesion/enunciate/samples/docs/CountEnum.java
@@ -0,0 +1,24 @@
+package com.webcohesion.enunciate.samples.docs;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+/**
+ * Just an enum
+ */
+@JsonInclude
+public enum CountEnum {
+    /**
+     * <p>Here is an enum for a value that comes first.</p>
+     */
+    FIRST,
+
+    /**
+     * <p>Here is an enum for a value that comes second.</p>
+     */
+    SECOND,
+
+    /**
+     * <p>Here is an enum for a value that comes third.</p>
+     */
+    THIRD,
+}
