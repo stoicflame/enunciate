59dc6a70950e748aa773c5f32b398a1aed71526a
Ryan Heaton <ryan@webcohesion.com>
2017-10-06 14:55:02 -0600

    applying some caching for performance improvements per #706

--- lombok/src/main/java/com/webcohesion/enunciate/modules/lombok/LombokDecoration.java
+++ lombok/src/main/java/com/webcohesion/enunciate/modules/lombok/LombokDecoration.java
@@ -1,18 +1,31 @@
 package com.webcohesion.enunciate.modules.lombok;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import com.webcohesion.enunciate.javac.decorations.DecoratedProcessingEnvironment;
 import com.webcohesion.enunciate.javac.decorations.ElementDecoration;
 import com.webcohesion.enunciate.javac.decorations.element.DecoratedElement;
+import com.webcohesion.enunciate.javac.decorations.element.DecoratedExecutableElement;
 import com.webcohesion.enunciate.javac.decorations.element.DecoratedTypeElement;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.SimpleElementVisitor6;
 
 /**
  * @author Ryan Heaton
  */
 public class LombokDecoration extends SimpleElementVisitor6<Void, DecoratedProcessingEnvironment> implements ElementDecoration {
+
+  private Map<String, List<DecoratedExecutableElement>> CACHE = new TreeMap<String, List<DecoratedExecutableElement>>();
 
   @Override
   public void applyTo(DecoratedElement e, DecoratedProcessingEnvironment env) {
@@ -22,8 +35,8 @@ public class LombokDecoration extends SimpleElementVisitor6<Void, DecoratedProce
   @Override
   public Void visitType(TypeElement e, DecoratedProcessingEnvironment env) {
     DecoratedTypeElement typeElement = (DecoratedTypeElement) e;
-    LombokMethodGenerator lombokMethodGenerator = new LombokMethodGenerator(typeElement, env);
-    lombokMethodGenerator.generateLombokGettersAndSetters();
+    List<DecoratedExecutableElement> methods = getLombokMethodDecorations(typeElement, env);
+    typeElement.getMethods().addAll(methods);
     return null;
   }
 
@@ -33,5 +46,73 @@ public class LombokDecoration extends SimpleElementVisitor6<Void, DecoratedProce
     return null;
   }
 
+  private List<DecoratedExecutableElement> getLombokMethodDecorations(DecoratedTypeElement element, DecoratedProcessingEnvironment env) {
+    List<DecoratedExecutableElement> methods = CACHE.get(element.getQualifiedName().toString());
+
+    if (methods == null) {
+      methods = new ArrayList<DecoratedExecutableElement>();
+      CACHE.put(element.getQualifiedName().toString(), methods);
+
+      List<? extends VariableElement> fields = element.getFields();
+      for (VariableElement field : fields) {
+        if (shouldGenerateGetter(element, field)) {
+          methods.add(new DecoratedExecutableElement(new LombokGeneratedGetter(field, env), env));
+        }
+        if (shouldGenerateSetter(element, field)) {
+          methods.add(new DecoratedExecutableElement(new LombokGeneratedSetter(field, env), env));
+        }
+      }
+    }
+
+    return methods;
+  }
+
+  private boolean shouldGenerateGetter(DecoratedTypeElement element, Element field) {
+    String fieldSimpleName = field.getSimpleName().toString();
+    for (ExecutableElement method : element.getMethods()) {
+      DecoratedExecutableElement decoratedMethod = (DecoratedExecutableElement) method;
+      if (decoratedMethod.getPropertyName() != null && decoratedMethod.getPropertyName().equals(fieldSimpleName) && decoratedMethod.isGetter()) {
+        return false;
+      }
+    }
+
+    if (field.getAnnotation(Getter.class) != null) {
+      return true;
+    }
+
+    if (element.getAnnotation(Getter.class) != null) {
+      return true;
+    }
+
+    if (element.getAnnotation(Data.class) != null) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private boolean shouldGenerateSetter(DecoratedTypeElement element, Element field) {
+    String fieldSimpleName = field.getSimpleName().toString();
+    for (ExecutableElement method : element.getMethods()) {
+      DecoratedExecutableElement decoratedMethod = (DecoratedExecutableElement) method;
+      if (decoratedMethod.getPropertyName() != null && decoratedMethod.getPropertyName().equals(fieldSimpleName) && decoratedMethod.isSetter()) {
+        return false;
+      }
+    }
+
+    if (field.getAnnotation(Setter.class) != null) {
+      return true;
+    }
+
+    if (element.getAnnotation(Setter.class) != null) {
+      return true;
+    }
+
+    if (element.getAnnotation(Data.class) != null) {
+      return true;
+    }
+
+    return false;
+  }
 
 }
