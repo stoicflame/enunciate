0e43730248e753f8aca210f0ea32d02247b84523
Alex Panchenko <alex.panchenko@gmail.com>
2018-04-22 22:59:37 +0700

    Revert "850 for a class the @JsonTypeInfo is now resolved also from super-classes"
    This reverts commit a40df500f2e49f447c606886f614fb3ca1245be5.

--- jackson1/src/main/java/com/webcohesion/enunciate/modules/jackson1/model/TypeDefinition.java
+++ jackson1/src/main/java/com/webcohesion/enunciate/modules/jackson1/model/TypeDefinition.java
@@ -23,7 +23,6 @@ import com.webcohesion.enunciate.javac.decorations.element.*;
 import com.webcohesion.enunciate.javac.decorations.type.TypeMirrorUtils;
 import com.webcohesion.enunciate.metadata.ClientName;
 import com.webcohesion.enunciate.modules.jackson1.EnunciateJackson1Context;
-import com.webcohesion.enunciate.util.AnnotationUtils;
 import com.webcohesion.enunciate.util.SortedList;
 import org.codehaus.jackson.annotate.*;
 
@@ -483,9 +482,7 @@ public abstract class TypeDefinition extends DecoratedTypeElement implements Has
   public String getTypeIdProperty() {
     String property = null;
 
-    List<JsonTypeInfo> annotations = AnnotationUtils.getAnnotations(JsonTypeInfo.class, this);
-    JsonTypeInfo typeInfo = annotations.isEmpty() ? null : annotations.get(0);
-
+    JsonTypeInfo typeInfo = getAnnotation(JsonTypeInfo.class);
     if (typeInfo != null) {
       property = typeInfo.property();
       if ("".equals(property)) {
