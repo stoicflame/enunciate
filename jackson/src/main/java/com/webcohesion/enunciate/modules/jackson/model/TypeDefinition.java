3e485c097b4144a73b1ab3151274325b93d98883
Steven Schlansker <stevenschlansker@gmail.com>
2019-03-28 13:21:19 -0700

    jackson: respect @JsonSerialize(as) for property detection

--- jackson/src/main/java/com/webcohesion/enunciate/modules/jackson/model/TypeDefinition.java
+++ jackson/src/main/java/com/webcohesion/enunciate/modules/jackson/model/TypeDefinition.java
@@ -16,6 +16,8 @@
 package com.webcohesion.enunciate.modules.jackson.model;
 
 import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.webcohesion.enunciate.EnunciateException;
 import com.webcohesion.enunciate.facets.Facet;
 import com.webcohesion.enunciate.facets.HasFacets;
@@ -37,7 +39,11 @@ import javax.lang.model.util.ElementFilter;
 import javax.lang.model.util.Types;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
+
+import java.lang.annotation.Annotation;
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * A json type definition.
@@ -567,19 +573,45 @@ public abstract class TypeDefinition extends DecoratedTypeElement implements Has
       super(env);
     }
 
+    private <A extends Annotation> DecoratedExecutableElement refine(DecoratedExecutableElement executable, Class<A> annotation, Function<A, Class<?>> refiner) {
+      Element elt = executable;
+      while (elt != null && elt.getKind() != ElementKind.CLASS && elt.getKind() != ElementKind.INTERFACE) {
+        elt = elt.getEnclosingElement();
+      }
+      if (elt == null) {
+        return executable;
+      }
+      final A js = env.getElementUtils().getTypeElement(((DecoratedTypeElement)elt).getQualifiedName()).getAnnotation(annotation);
+
+      if (js != null) {
+        DeclaredType as = (DeclaredType) Annotations.mirrorOf(() -> refiner.apply(js), env, Void.class);
+        if (as == null) {
+          return executable;
+        }
+        for (Element elem : as.asElement().getEnclosedElements()) {
+          if (elem.getSimpleName().equals(executable.getSimpleName()) && elem instanceof ExecutableElement) {
+            return new DecoratedExecutableElement((ExecutableElement) elem, env); 
+          }
+        }
+      }
+      return executable;
+    }
+
     @Override
     public boolean isGetter(DecoratedExecutableElement executable) {
+      executable = refine(executable, JsonSerialize.class, JsonSerialize::as);
       return executable.isGetter() || (executable.getParameters().isEmpty() && (executable.getAnnotation(JsonProperty.class) != null || executable.getAnnotation(JsonValue.class) != null));
     }
 
     @Override
     public boolean isSetter(DecoratedExecutableElement executable) {
+      executable = refine(executable, JsonDeserialize.class, JsonDeserialize::as);
       return executable.isSetter() || (executable.getParameters().size() == 1 && (executable.getAnnotation(JsonProperty.class) != null || executable.getAnnotation(JsonValue.class) != null));
     }
 
     @Override
     public String getPropertyName(DecoratedExecutableElement method) {
-      JsonProperty jsonProperty = method.getAnnotation(JsonProperty.class);
+      JsonProperty jsonProperty = refine(method, JsonSerialize.class, JsonSerialize::as).getAnnotation(JsonProperty.class);
       if (jsonProperty != null) {
         String propertyName = jsonProperty.value();
         if (!propertyName.isEmpty()) {
