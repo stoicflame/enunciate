ab093f892e824f392b93d9fcc2651a6f0708073f
Ryan Heaton <ryan@webcohesion.com>
2020-04-24 11:22:09 -0600

    removing the simple archetype in favor of the GH sample app

--- simple-archetype/src/main/resources/archetype-resources/src/main/java/domain/link/package-info.java
+++
@@ -1,9 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-@XmlSchema (
-  namespace = "http://api.example.com/link"
-)
-package ${package}.domain.link;
-
-import javax.xml.bind.annotation.XmlSchema;
--- simple-archetype/src/main/resources/archetype-resources/src/main/java/domain/persona/package-info.java
+++
@@ -1,9 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-@XmlSchema (
-  namespace = "http://api.example.com/persona"
-)
-package ${package}.domain.persona;
-
-import javax.xml.bind.annotation.XmlSchema;
