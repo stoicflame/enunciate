cfeae4f436a7b7243c895c45a4ca416ef95065e8
Tamas Cservenak <tamas@cservenak.net>
2023-03-20 21:24:30 +0100

    Update build
    Changes:
    * update build plugins
    * fix reporting (is a bit funky currently)
    * add some notes/TODOs

--- slim-maven-plugin/src/main/java/com/webcohesion/enunciate/mojo/DocsBaseMojo.java
+++ slim-maven-plugin/src/main/java/com/webcohesion/enunciate/mojo/DocsBaseMojo.java
@@ -101,7 +101,7 @@ public class DocsBaseMojo extends ConfigMojo implements MavenReport {
   }
 
   @Override
-  public void generate(Sink sink, Locale locale) throws MavenReportException {
+  public void generate(org.codehaus.doxia.sink.Sink sink, Locale locale) throws MavenReportException {
     if (this.siteError != null) {
       throw new MavenReportException("Unable to generate Enunciate documentation.", this.siteError);
     }
